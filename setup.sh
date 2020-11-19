5b46e0b22f73e10a9987baf7e58d961711516a17
fjimenez81 <fernancuerva@gmail.com>
2020-11-19 19:57:46 +0100

    ready to up

--- setup.sh
+++ setup.sh
@@ -8,36 +8,44 @@ PURPLE="\e[95m"
 CYAN="\e[96m"
 WHITE="\e[97m"
 
-minikube delete
-minikube config unset vm-driver
+#minikube delete
+#minikube config unset vm-driver
 
 # Detect the platform (similar to $OSTYPE)
 OS="`uname`"
 
+# minikube start --driver=docker --cpus=2
+# sed -i "s/xxxx-xxxx/172.17.0.10-172.17.0.19/g" srcs/metallb.yaml
+# sed -i "s/zzzz-zzzz/172.17.0.20-172.17.0.20/g" srcs/metallb.yaml
+# sed -i "s/WP_IP/172.17.0.20:5050/g" srcs/mysql/wordpress.sql
+# sed -i "s/yyyy-yyyy/172.17.0.21-172.17.0.21/g" srcs/metallb.yaml
+# sed -i "s/FTPS_IP/172.17.0.21/g" srcs/ftps/vsftpd.conf
+
+
+
 case $OS in
-	"Linux")
-		minikube start --driver=docker --cpus=2
-		sed -i "s/xxxx-xxxx/172.17.0.10-172.17.0.19/g" srcs/metallb.yaml
-		sed -i "s/zzzz-zzzz/172.17.0.20-172.17.0.20/g" srcs/metallb.yaml
-		sed -i "s/WP_IP/172.17.0.20:5050/g" srcs/mysql/wordpress.sql
-		sed -i "s/yyyy-yyyy/172.17.0.21-172.17.0.21/g" srcs/metallb.yaml
-		sed -i "s/FTPS_IP/172.17.0.21/g" srcs/ftps/vsftpd.conf
-	;;
+ 	"Linux")
+ 		minikube start --driver=virtualbox
+        sed -i "s/xxxx-xxxx/192.168.99.110-192.168.99.119/g" srcs/metallb.yaml
+        sed -i "s/zzzz-zzzz/192.168.99.120-192.168.99.120/g" srcs/metallb.yaml
+        sed -i "s/WP_IP/192.168.99.120:5050/g" srcs/mysql/wordpress.sql
+        sed -i "s/yyyy-yyyy/192.168.99.121-192.168.99.121/g" srcs/metallb.yaml
+        sed -i "s/FTPS_IP/192.168.99.121/g" srcs/ftps/vsftpd.conf
+ 	;;
 	"Darwin")
-		minikube start --driver=virtualbox
-		sed -i '' "s/xxxx-xxxx/192.168.99.110-192.168.99.119/g" srcs/metallb.yaml
+ 		minikube start --driver=virtualbox
+ 		sed -i '' "s/xxxx-xxxx/192.168.99.110-192.168.99.119/g" srcs/metallb.yaml
 		sed -i '' "s/zzzz-zzzz/192.168.99.120-192.168.99.120/g" srcs/metallb.yaml
-		sed -i '' "s/WP_IP/192.168.99.120:5050/g" srcs/mysql/wordpress.sql
-		sed -i '' "s/yyyy-yyyy/192.168.99.121-192.168.99.121/g" srcs/metallb.yaml
-		sed -i '' "s/FTPS_IP/192.168.99.121/g" srcs/ftps/vsftpd.conf
-	;;
-	*) ;;
-esac
+ 		sed -i '' "s/WP_IP/192.168.99.120:5050/g" srcs/mysql/wordpress.sql
+ 		sed -i '' "s/yyyy-yyyy/192.168.99.121-192.168.99.121/g" srcs/metallb.yaml
+ 		sed -i '' "s/FTPS_IP/192.168.99.121/g" srcs/ftps/vsftpd.conf
+ 	;;
+ 	*) ;;
+ esac
 
 echo -en $YELLOW
 echo "MINIKUBE STARTING..."
 
-#minikube start --driver=docker --cpus=2
 minikube addons enable metrics-server
 minikube addons enable dashboard &> /dev/null
 minikube addons enable metallb
@@ -77,40 +85,56 @@ kubectl get svc
 echo -en $CYAN
 
 echo "=======COMMANDS======="
-echo "ssh -p 22 ssh_admin@ip_svc"
+echo "NGINX"
+echo 'curl -I http://$IP-NGINX-SERVICE'
+echo "-----------------------"
+echo 'SSH'
+echo 'ssh -p 22 ssh_admin@$IP-NGINX-SERVICE'
 echo "ssh: ssh_admin--9090"
 echo "-----------------------"
-echo "ftp -p 172.17.0.21"
-echo "ftp: ftp_admin--01010"
+echo 'FTPS:'
+echo 'User: ftps_user, Password: password'
+echo 'curl --ftp-ssl --insecure --user ftp_admin:01010 ftp://192.168.99.121:21'
+echo 'To test upload/load file:'
+echo 'touch filetest'
+echo 'Send file: - curl -T filetest --ftp-ssl --insecure --user ftp_admin:01010 ftp://192.168.99.121:21'
+echo 'Delete file: - rm filetest Retrieve file: - curl -O --ftp-ssl --insecure --user ftp_admin:01010 ftp://192.168.99.121:21/filetest'
 echo "-----------------------"
-echo "nginx"
-#echo "http://172.17.0.2"
-echo "-----------------------"
-echo "wordpress"
+echo "WORDPRESS"
 #echo "http://172.17.0.3:5050"
 echo "wordpress: ferfox_1--ferfox_1"
 echo "wordpress: user_1--2020"
 echo "wordpress: user_2--2121"
 echo "-----------------------"
-echo "phpmyadmin"
+echo "PHPMYADMIN"
 #echo "http://172.17.0.4:5000"
 echo "phpmyadmin: ferfox--1234"
 echo "-----------------------"
-echo "grafana"
+echo "GRAFANA"
 #echo "http://172.17.0.6:3000"
 echo "grafana: admin--admin"
 echo "-----------------------"
 
+
+# LINUX 
+# sed -i "s/172.17.0.10-172.17.0.19/xxxx-xxxx/g" srcs/metallb.yaml
+# sed -i "s/172.17.0.20-172.17.0.20/zzzz-zzzz/g" srcs/metallb.yaml
+# sed -i "s/172.17.0.20:5050/WP_IP/g" srcs/mysql/wordpress.sql
+# sed -i "s/172.17.0.21-172.17.0.21/yyyy-yyyy/g" srcs/metallb.yaml
+# sed -i "s/172.17.0.21/FTPS_IP/g" srcs/ftps/vsftpd.conf
+
+
+
 case $OS in
 	"Linux")
-		sed -i "s/172.17.0.10-172.17.0.19/xxxx-xxxx/g" srcs/metallb.yaml
-        sed -i "s/172.17.0.20-172.17.0.20/zzzz-zzzz/g" srcs/metallb.yaml
-        sed -i "s/172.17.0.20:5050/WP_IP/g" srcs/mysql/wordpress.sql
-        sed -i "s/172.17.0.21-172.17.0.21/yyyy-yyyy/g" srcs/metallb.yaml
-        sed -i "s/172.17.0.21/FTPS_IP/g" srcs/ftps/vsftpd.conf
+		sed -i "s/192.168.99.110-192.168.99.119/xxxx-xxxx/g" srcs/metallb.yaml
+        sed -i "s/192.168.99.120-192.168.99.120/zzzz-zzzz/g" srcs/metallb.yaml
+        sed -i "s/192.168.99.120:5050/WP_IP/g" srcs/mysql/wordpress.sql
+        sed -i "s/192.168.99.121-192.168.99.121/yyyy-yyyy/g" srcs/metallb.yaml
+        sed -i "s/192.168.99.121/FTPS_IP/g" srcs/ftps/vsftpd.conf
 	;;
 	"Darwin")
-		minikube start --driver=virtualbox
+
 		sed -i '' "s/192.168.99.110-192.168.99.119/xxxx-xxxx/g" srcs/metallb.yaml
 		sed -i '' "s/192.168.99.120-192.168.99.120/zzzz-zzzz/g" srcs/metallb.yaml
 		sed -i '' "s/192.168.99.120:5050/WP_IP/g" srcs/mysql/wordpress.sql
@@ -121,3 +145,11 @@ case $OS in
 esac
 
 
+# KILL PODS
+# kubectl exec deploy/nginx-deployment -- pkill nginx
+# kubectl exec deploy/wordpress-deployment -- pkill php
+# kubectl exec deploy/influxdb-deployment -- pkill influxd
+# kubectl exec deploy/phpmyadmin-deployment -- pkill php
+# kubectl exec deploy/grafana-deployment -- pkill grafana-server
+# kubectl exec deploy/ftps-deployment -- pkill vsftpd
+# kubectl exec deploy/mysql-deployment -- pkill mysql
